f8a59327f6a5736108b673a72f4f70729a847375
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2023-04-01 18:43:09 -0400

    comments begin

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,25 @@
-import { request } from "express";
 import { openDB } from "idb";
-
+// initalize the database
 const initdb = async () =>
+  // set database name to "jate version 1"
   openDB("jate", 1, {
     upgrade(db) {
+      // if database exists, return
       if (db.objectStoreNames.contains("jate")) {
         console.log("jate database already exists");
         return;
       }
+      // if no database sets up schema
       db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
       console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+// updates the data when called
 export const putDb = async (content) => {
   // find the correct Database and read the data
+  //
   const jateDb = await openDB("jaet", 1);
   const tr = jateDb.transaction("jate", "readwrite");
   const storage = tr.objectStore("jate");
